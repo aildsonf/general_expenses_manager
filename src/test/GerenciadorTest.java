8c2d19de9154e90340883c1e2e5b9f55f3f0972b
alan.marinho <alan.marinho@N000001.parvi.local>
2020-10-13 18:02:52 -0300

    modificação classe test

--- src/test/GerenciadorTest.java
+++ src/test/GerenciadorTest.java
@@ -2,5 +2,30 @@ import junit.framework.TestCase;
 
 public class GerenciadorTest extends TestCase {
 	
-	public void 
+	public void testInsereUsuario(Usuario u) {
+		
+	}
+	
+	public void testRemoveUsuario() {
+		
+	}
+	
+	public void testInsereSaldo(double valor) {
+		
+	}
+	
+	public void testAtualizaSaldo(double valor) {
+		
+	}
+	
+	public void testInsereDespesa(ItemDespesa d) {
+		
+	}
+	
+	public void testRemoveDespesa(ItemDespesa d) {
+		
+	}
+	
+	public void testMostraExtrato()
+	
 }
